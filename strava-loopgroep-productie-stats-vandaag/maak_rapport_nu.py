b48554428f72bee8dd44c695531466f6f304281f
CoderBe98 <peperniels@hotmail.com>
2026-06-25 13:18:45 +0200

    Delete strava-loopgroep-productie-stats-vandaag directory

--- strava-loopgroep-productie-stats-vandaag/maak_rapport_nu.py
+++
@@ -1,9 +0,0 @@
-from config import get_settings
-from database import initialise
-from report_service import run_report
-
-if __name__ == "__main__":
-    settings = get_settings()
-    initialise(settings.database_path)
-    report = run_report(settings)
-    print(report.content)
--- strava-loopgroep-productie-stats-vandaag/run_tests.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-python test_groepsapp.py
-pause
